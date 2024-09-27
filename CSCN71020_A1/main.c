7480aca80bdb49271544f641c3ce69affd271635
Anderson Rippon Raj <andersonraj@yahoo.com>
2024-09-27 16:36:29 -0400

    Commit #3 - Resolved error in the subtract() function

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,6 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
 
 void main() {
@@ -20,7 +21,10 @@ void main() {
 	{
 	case 1:
 		add();
+	case 2:
+		subtract();
 	}
+	
 
 }
 
@@ -44,4 +48,15 @@ void add() {
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract() {
+
+	double num1, num2, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
